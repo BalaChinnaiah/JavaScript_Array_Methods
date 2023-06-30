94f52b56ce90f6e4579e42004dbcdf274bcf81ba
Bala Chinnaiah <97093618+BalaChinnaiah@users.noreply.github.com>
2023-06-30 17:05:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// Array Methods
+let a = [1,2,3,4,5]
+
+let b = a.toString()
+console.log(typeof b)
+
+let c = a.join("_")
+console.log(c)
+
+console.log(a.pop())
+console.log(a)
+
+a.push(100) 
+console.log(a)
+
+console.log(a.shift())
+console.log(a.unshift(10))
+console.log(a)
